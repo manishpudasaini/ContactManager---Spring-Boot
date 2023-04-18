9d6b319f5c1f7190d9e8e3133d67492a95890a7f
manishpudasaini <eziomanish1@gmail.com>
2023-04-18 16:44:21 +0545

    Contact Manager - commit 1

--- src/main/java/com/contactmanager/contactmanager/controller/MyController.java
+++ src/main/java/com/contactmanager/contactmanager/controller/MyController.java
@@ -1,12 +1,11 @@
 package com.contactmanager.contactmanager.controller;
 
-import com.contactmanager.contactmanager.entity.Contact;
-import com.contactmanager.contactmanager.entity.User;
 import com.contactmanager.contactmanager.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class MyController {
@@ -16,7 +15,7 @@ public class MyController {
         this.userRepository=userRepository;
     }
 
-    @GetMapping("/home")
+    @GetMapping("/")
     public String home(){
         return "home";
     }
